1e69eb5adef4930c4f6e85530e0e3cf81b66d9e9
safari <rsafari.s@gmail.com>
2018-09-28 00:17:06 -0500

    initial commit

---
+++ test/game_test.exs
@@ -0,0 +1,77 @@
+defmodule GameTest do
+  use ExUnit.Case
+
+  alias Hangman.Game
+
+  test "new_game returns structure" do
+    game = Game.new_game()
+
+    assert game.turns_left == 7
+    assert game.game_state == :initializing
+    assert length(game.letters) > 0
+  end
+
+  test "state isn't changed for :won or :lost game" do
+    for state <- [ :won, :lost ] do
+      game = Game.new_game |> Map.put(:game_state, state)
+      assert ^game = Game.make_move(game, "x")
+    end
+  end
+
+  test "first occurrence of letter is not already used" do
+    game = Game.new_game |> Game.make_move("x")
+    assert game.game_state != :already_used
+  end
+
+  test "second occurrence of letter is not already used" do
+    game =
+      Game.new_game
+      |> Game.make_move("x")
+      |> Game.make_move("x")
+
+    assert game.game_state == :already_used
+  end
+
+  test "a good guess is recognized" do
+    game =
+      Game.new_game("wribble")
+      |> Game.make_move("w")
+
+    assert game.game_state == :good_guess
+    assert game.turns_left == 7
+  end
+
+  test "a guessed word is a won game" do
+    word = "wribble"
+    game =
+      word
+      |> String.codepoints
+      |> Enum.reduce(Game.new_game(word), fn c, game ->
+        Game.make_move(game, c)
+      end)
+
+    assert game.game_state == :won
+    assert game.turns_left == 7
+  end
+
+  test "bad guess is recognized" do
+    game =
+      Game.new_game("wibble")
+      |> Game.make_move("x")
+
+    assert game.game_state == :bad_guess
+    assert game.turns_left == 6
+  end
+
+  test "lost game is recognized" do
+    game =
+      1..7
+      |> Enum.reduce(Game.new_game("wribble"), fn c, game ->
+        Game.make_move(game, c)
+      end)
+
+    assert game.game_state == :lost
+    assert game.turns_left == 0
+  end
+
+end
